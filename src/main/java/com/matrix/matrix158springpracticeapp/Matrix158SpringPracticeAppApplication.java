e34152b213afe2afc88410778762b0441338e81c
imranrasulzade <imranrasulzade5@gmail.com>
2024-03-18 20:21:51 +0400

    git initalize first time

---
+++ src/main/java/com/matrix/matrix158springpracticeapp/Matrix158SpringPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.matrix158springpracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Matrix158SpringPracticeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Matrix158SpringPracticeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matrix/matrix158springpracticeapp/Matrix158SpringPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.matrix158springpracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Matrix158SpringPracticeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
