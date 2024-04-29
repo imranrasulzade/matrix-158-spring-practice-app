eaffd8ef82d3ab426cc2d96c62e50fd6021b84b4
Azmammad <ezmemmedcebrayilov17@gmail.com>
2024-04-29 20:51:00 +0400

    change userrepository class->interface

--- src/main/java/com/matrix/matrix158springpracticeapp/repository/UserRepository.java
+++ src/main/java/com/matrix/matrix158springpracticeapp/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.matrix.matrix158springpracticeapp.repository;public class UserRepository {
+package com.matrix.matrix158springpracticeapp.repository;
+
+import com.matrix.matrix158springpracticeapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
 }
