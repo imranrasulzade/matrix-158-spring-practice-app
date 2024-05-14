1e796249d413ce770f48b8846834f9645f6ace23
sabiaydin <sabinamammadova871@gmail.com>
2024-05-14 20:00:00 +0400

    group and social media added

--- src/main/java/com/matrix/matrix158springpracticeapp/repository/GroupRepository.java
+++ src/main/java/com/matrix/matrix158springpracticeapp/repository/GroupRepository.java
@@ -1,2 +1,7 @@
-package com.matrix.matrix158springpracticeapp.repository;public interface GroupRepository {
+package com.matrix.matrix158springpracticeapp.repository;
+
+import com.matrix.matrix158springpracticeapp.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group,Integer> {
 }
