2a9d2ea2675437df4c8e6cc260cf3c5f5bddd6f5
ilgarmv <b053162022@asoiu.edu.az>
2024-05-14 11:53:57 +0400

    task completed

---
+++ src/main/java/com/matrix/matrix158springpracticeapp/service/implementation/CompanyServiceImpl.java
@@ -0,0 +1,39 @@
+package com.matrix.matrix158springpracticeapp.service.implementation;
+
+import com.matrix.matrix158springpracticeapp.entity.Company;
+import com.matrix.matrix158springpracticeapp.repository.CompanyRepository;
+import com.matrix.matrix158springpracticeapp.service.interfaces.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+
+    @Override
+    public Company findById(Integer id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Company> findAll() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public Company save(Company company) {
+        return companyRepository.save(company);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        companyRepository.deleteById(id);
+    }
+
+}
