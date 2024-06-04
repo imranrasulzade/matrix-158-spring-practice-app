5b4390c9905bfbcda372f124b10bacdcb01c7113
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2024-06-04 13:51:42 +0400

    studentController is updated

--- src/main/java/com/matrix/matrix158springpracticeapp/controller/StudentController.java
+++ src/main/java/com/matrix/matrix158springpracticeapp/controller/StudentController.java
@@ -1,16 +1,46 @@
 package com.matrix.matrix158springpracticeapp.controller;
 
+import com.matrix.matrix158springpracticeapp.dto.request.StudentRequestDto;
 import com.matrix.matrix158springpracticeapp.dto.response.StudentResponseDto;
+import com.matrix.matrix158springpracticeapp.service.interfaces.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentController {
-    public StudentResponseDto get(){
-        return null;
+
+    private final StudentService studentService;
+
+    @GetMapping("/{id}")
+    public StudentResponseDto findById(@PathVariable Integer id){
+        return studentService.findById(id);
     }
+
+    @GetMapping
+    public List<StudentResponseDto> findAll(){
+        return studentService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public StudentResponseDto save(@RequestBody StudentRequestDto studentRequestDto){
+        return studentService.save(studentRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public StudentResponseDto update(@RequestBody StudentRequestDto studentRequestDto,@PathVariable Integer id){
+        return studentService.update(studentRequestDto,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id){
+        studentService.deleteById(id);
+    }
+
 }
