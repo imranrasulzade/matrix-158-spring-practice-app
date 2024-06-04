163194f4b8955b98f8a52807f9993c5b1587e5af
Azmammad <ezmemmedcebrayilov17@gmail.com>
2024-06-04 10:52:00 +0400

    i write teacher service update,delete and getAll methods and create teacher controller

--- src/main/java/com/matrix/matrix158springpracticeapp/controller/TeacherController.java
+++ src/main/java/com/matrix/matrix158springpracticeapp/controller/TeacherController.java
@@ -1,11 +1,14 @@
 package com.matrix.matrix158springpracticeapp.controller;
 
 import com.matrix.matrix158springpracticeapp.dto.request.TeacherRequestDto;
+import com.matrix.matrix158springpracticeapp.dto.request.UserRequestDto;
 import com.matrix.matrix158springpracticeapp.dto.response.TeacherResponseDto;
 import com.matrix.matrix158springpracticeapp.service.interfaces.TeacherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,13 +17,31 @@ public class TeacherController {
     private final TeacherService service;
 
     @GetMapping("{id}")
-    public TeacherResponseDto getById(@PathVariable Integer id){
+    public TeacherResponseDto findById(@PathVariable Integer id){
         return service.getById(id);
+    }
+
+    @GetMapping
+    public List<TeacherResponseDto> getAll(){
+        return service.getAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void getById(@RequestBody TeacherRequestDto teacherRequestDto){
-        service.add(teacherRequestDto);
+    public TeacherResponseDto add(@RequestBody TeacherRequestDto teacherRequestDto){
+        return service.add(teacherRequestDto);
     }
+
+    @PutMapping("/{id}")
+    public TeacherResponseDto update(@PathVariable Integer id, @RequestBody TeacherRequestDto teacherRequestDto){
+        return service.update(id,teacherRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        service.delete(id);
+    }
+
+
+
 }
