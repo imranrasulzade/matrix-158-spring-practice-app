733ef202d783e4bc43beca99e558e9cec4ce9dee
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2024-06-04 13:52:02 +0400

    studentMapper is updated

--- src/main/java/com/matrix/matrix158springpracticeapp/mapper/StudentMapper.java
+++ src/main/java/com/matrix/matrix158springpracticeapp/mapper/StudentMapper.java
@@ -5,12 +5,14 @@ import com.matrix.matrix158springpracticeapp.dto.response.StudentResponseDto;
 import com.matrix.matrix158springpracticeapp.entity.Student;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public abstract class StudentMapper {
 
     @Mapping(source = "user.name", target = "name")
     @Mapping(source = "user.surname", target = "surname")
     public abstract StudentResponseDto toDTO(Student student);
-    public abstract Student toEntity (StudentRequestDto studentRequestDto);
+    public abstract Student toEntity (@MappingTarget Student student, StudentRequestDto studentRequestDto);
 }
